227fdebb8bc5dc78d0763ea231cabb9bcf333a6d
René Nitzsche <rene@system25.de>
2024-06-10 20:30:46 +0200

    Allow install in T3 12 #3

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,13 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
+if (!(defined('TYPO3') || defined('TYPO3_MODE'))) {
     exit('Access denied.');
 }
 $_EXTKEY = 'dfbsync';
 
 // Hook for competition search
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cfc_league']['search_Competition_getTableMapping_hook'][] = 'System25\T3sports\DfbSync\Hook\Search->getTableMappingCompetition';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cfc_league']['search_Competition_getJoins_hook'][] = 'System25\T3sports\DfbSync\Hook\Search->getJoinsCompetition';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cfc_league']['search_Competition_getTableMapping_hook'][] = System25\T3sports\DfbSync\Hook\Search::class.'->getTableMappingCompetition';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['cfc_league']['search_Competition_getJoins_hook'][] = System25\T3sports\DfbSync\Hook\Search::class.'->getJoinsCompetition';
 
 // Scheduler für Sync
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\System25\T3sports\DfbSync\Scheduler\SyncTask::class] = [
